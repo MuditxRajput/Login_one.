cdc0a8fb28a2c0892e3d73ab0a2c11aeaf78b688
MuditRajput2001 <you@example.commuditrajputpersonal@gmail>
2023-09-19 01:09:15 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,75 @@ import React, { useState } from 'react';
 
 
 const App=()=>{
-    
-const[name,setName] = useState("");
-const[fullName , setFullName] = useState();
-const InputEvent =(e)=>{
-   setName(e.target.value);
-}
-const changes =()=>{
-    setFullName(name);
-}
+    const[fullName , setFullName] = useState({
+        fname:"",
+        lname:"",
+        email:"",
+    });
+    // const[] = useState();
+    const InputEvent = (e)=>{
+        
+        const value = e.target.value;
+        const name = e.target.name;
+
+        setFullName((preValue)=>{
+            if(name==="fName")
+            {
+                return{
+                    fname:value,
+                    lname : preValue.lname,
+                    email:preValue.email,
+
+                }
+                
+            }
+            else if(name=='lName'){
+                return{
+
+                    fname : preValue.fname,
+                    lname : value,
+                    email:preValue.email,
+                }
+            }
+            else{
+                return{
+                    fname:preValue.fname,
+                    lname:preValue.lname,
+                    email:value,
+                }
+            }
+        })
+
+    }
+    const submit = ()=>{
+
+    }
+
+
     return(
         <>
         <div className='input-box'>
 
-        <h1>{"Hello "}{fullName}</h1>
+        <h1>{`Hello ${fullName.fname} ${fullName.lname}`}</h1>
+        <hr></hr>
+        <h3 className='email'>{fullName.email}</h3>
         <input type="text" placeholder='Enter your Name'
         className='text-input' onChange={InputEvent}
-        value={name} />
-        {/* <input type="text" placeholder='Enter your Password'
-        className='text-input' onChange={pass} /> */}
+        name ="fName" value={fullName.fName}
+         />
+        <input type="text" placeholder='Enter your LastName'
+        className='text-input' onChange={InputEvent}
+        name = "lName" value={fullName.lName}
+         />
+        <input type="text" placeholder='Enter your Email'
+        className='text-input' onChange={InputEvent}
+        name = "eMail" value={fullName.lName}
+         />
         
 
         <div className='Btn'>
 
-        <button onClick={changes} >Click me</button>
+        <button onClick={submit}  >Click me</button>
         </div>
         </div>
         </>
